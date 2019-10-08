93b2a66108ab39b46db565f8aa08e544046b4147
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2019-10-08 21:50:18 +0900

    ログインシステムの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :users, only: [:new, :create, :show]
 
   resources :feeds do
     collection do
