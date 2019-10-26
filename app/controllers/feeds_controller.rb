e32b8c5ca626a3a8a7f6de676bf62501caa0a392
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2019-10-26 23:46:18 +0900

    アクセス制限機能&詳細画面の画像の大きさ調整

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,6 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user, only: [:edit, :update, :destroy]
 
   # GET /feeds
   # GET /feeds.json
@@ -83,5 +84,12 @@ class FeedsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
   def feed_params
     params.require(:feed).permit(:image, :image_cache, :content)
+  end
+
+  def authenticate_user
+    if @current_user == params[:id].to_i
+      flash[:notice] = "あなたの投稿では無いので操作できません"
+      redirect_to feeds_path
+    end
   end
 end
